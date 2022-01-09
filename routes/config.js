03cfe06132109d7842cf8f608aabf50a345fd269
Prashant <prashant@zero65.in>
2022-01-09 22:51:32 +0530

    Updating config

--- routes/config.js
+++ routes/config.js
@@ -1,3 +1,12 @@
+const userAccess = {
+  'antshpra@gmail.com':        ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
+  'gshiv009@gmail.com':        ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
+  'nextshivendra@gmail.com':   ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
+  'shivendraec1088@gmail.com': ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
+  'gupta.disha30@gmail.com':   ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
+  'kiteswear.jbp@gmail.com':   ['LV0248', 'IQ6382', 'NH6133', 'AJ8534']
+}
+
 module.exports = {
 
   market: {
@@ -6,7 +15,9 @@ module.exports = {
 
   zerodha: {
     '/session': {
-      auth: (email) => false
+      auth: (email, params) => {
+        return email && userAccess[email] && userAccess[email].indexOf(params.userId) != -1;
+      }
     }
   }
 
