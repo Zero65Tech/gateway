1944e03dd763d5c66431ae1bf3fd3101baafe7f1
Prashant <prashant@zero65.in>
2022-01-11 11:38:49 +0530

    Updating config

--- routes/config.js
+++ routes/config.js
@@ -4,7 +4,8 @@ const userAccess = {
   'nextshivendra@gmail.com':   ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
   'shivendraec1088@gmail.com': ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
   'gupta.disha30@gmail.com':   ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
-  'kiteswear.jbp@gmail.com':   ['LV0248', 'IQ6382', 'NH6133', 'AJ8534']
+  'kiteswear.jbp@gmail.com':   ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
+  'cli-prashant@zero65.iam.gserviceaccount.com': ['LV0248', 'IQ6382', 'NH6133', 'AJ8534']
 }
 
 module.exports = {
@@ -22,6 +23,23 @@ module.exports = {
         return email && userAccess[email] && userAccess[email].indexOf(params.userId) != -1;
       }
     }
+  },
+
+  invest: {
+    '/users/profiles': {},
+    '/users/groups': {},
+    '/trades': {},
+    '/trades/organized': {},
+    '/trades/fo/mom': {},
+    '/trades/pnl': {},
+    '/portfolio': {},
+    '/portfolio/yoy': {},
+    '/portfolio/scripts': {},
+    '/portfolio/performance': {},
+    '/portfolio/summary': {},
+    '/portfolio/symbol': {},
+    '/holding-gains/distribution': {},
+    '/fo/positions': {}
   }
 
 }
