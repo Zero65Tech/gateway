9e94507d890c11da425f55edacd02c61965ef069
Prashant <prashant@zero65.in>
2022-02-02 09:42:27 +0530

    Updating auth logic for zerodha

--- routes/services/zerodha.js
+++ routes/services/zerodha.js
@@ -1,3 +1,7 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const oAuth2Client = new OAuth2Client();
+
 const userAccess = {
   'antshpra@gmail.com':        ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
   'gshiv009@gmail.com':        ['LV0248', 'IQ6382', 'NH6133', 'AJ8534'],
@@ -8,19 +12,20 @@ const userAccess = {
   'cloud-run@zero65.iam.gserviceaccount.com': ['LV0248', 'IQ6382', 'NH6133', 'AJ8534']
 }
 
+async function auth(req) {
+  let token = req.headers.authorization;
+  if(token && token.startsWith('Bearer ')) {
+    let email = (await oAuth2Client.getTokenInfo(token.substring('Bearer '.length))).email;
+    return email && userAccess[email] && userAccess[email].indexOf(req.query.userId) != -1;
+  }
+  return false;
+}
+
 module.exports = {
   '/session': {
-    'GET': {
-      auth: (email, params) => {
-        return email && userAccess[email] && userAccess[email].indexOf(params.userId) != -1;
-      }
-    }
+    'GET': { auth: auth }
   },
   '/enctoken': {
-    'GET': {
-      auth: (email, params) => {
-        return email && userAccess[email] && userAccess[email].indexOf(params.userId) != -1;
-      }
-    }
+    'GET': { auth: auth }
   }
 }
