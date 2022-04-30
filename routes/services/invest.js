7125225a72acc599e272a7b2eae717e6c0e7f622
Prashant <prashant@zero65.in>
2022-04-30 11:56:35 +0530

    Updating auth logic for invest service

--- routes/services/invest.js
+++ routes/services/invest.js
@@ -1,34 +1,22 @@
 const { OAuth2Client } = require('google-auth-library');
-
 const { Service } = require('@zero65tech/common-utils');
-
 const oAuth2Client = new OAuth2Client();
 
+async function auth1(req) {
 
+  let token = req.headers.authorization || req.cookies.authorization;
+  if(token && token.startsWith('Bearer ')) {
+    req.query.account = token.length > 500 // TODO: Fix - Use ID tokens only
+      ? (await oAuth2Client.verifyIdToken({ idToken:token.substring('Bearer '.length), audience:'220251834863-p6gimkv0cgepodik4c1s8cs471dv9ioq.apps.googleusercontent.com' })).payload.email
+      : (await oAuth2Client.getTokenInfo(token.substring('Bearer '.length))).email;
+    return true;
+  } else {
+    return false;
+  }
 
-const DEMO_ACCOUNT = 'demo@zero65.in';
+}
 
-const paths = [
-  '/users/profiles',
-  '/users/groups',
-  '/trades',
-  '/trades/organized',
-  '/trades/fo/mom',
-  '/trades/pnl',
-  '/transactions',
-  '/transactions/funds',
-  '/sprints/trades',
-  '/portfolio',
-  '/portfolio/yoy',
-  '/portfolio/scripts',
-  '/portfolio/performance',
-  '/portfolio/summary',
-  '/portfolio/symbol',
-  '/holding-gains/distribution',
-  '/fo/positions'
-];
-
-async function auth(req) {
+async function auth2(req) {
 
   let token = req.headers.authorization || req.cookies.authorization;
   if(token && token.startsWith('Bearer ')) {
@@ -36,7 +24,7 @@ async function auth(req) {
       ? (await oAuth2Client.verifyIdToken({ idToken:token.substring('Bearer '.length), audience:'220251834863-p6gimkv0cgepodik4c1s8cs471dv9ioq.apps.googleusercontent.com' })).payload.email
       : (await oAuth2Client.getTokenInfo(token.substring('Bearer '.length))).email;
   } else {
-    req.query.account = DEMO_ACCOUNT;
+    req.query.account = 'demo@zero65.in';
   }
 
   if(req.query.profile) {
@@ -51,11 +39,22 @@ async function auth(req) {
 
 }
 
-module.exports = paths.reduce((obj, path) => {
-  obj[path] = {
-    'GET': {
-      auth: auth
-    }
-  };
-  return obj;
-}, {});
+module.exports = {
+  '/users/profiles':             { 'GET': { auth: auth1 } },
+  '/users/groups':               { 'GET': { auth: auth2 } },
+  '/trades':                     { 'GET': { auth: auth2 } },
+  '/trades/organized':           { 'GET': { auth: auth2 } },
+  '/trades/fo/mom':              { 'GET': { auth: auth2 } },
+  '/trades/pnl':                 { 'GET': { auth: auth2 } },
+  '/transactions':               { 'GET': { auth: auth2 } },
+  '/transactions/funds':         { 'GET': { auth: auth2 } },
+  '/sprints/trades':             { 'GET': { auth: auth2 } },
+  '/portfolio':                  { 'GET': { auth: auth2 } },
+  '/portfolio/yoy':              { 'GET': { auth: auth2 } },
+  '/portfolio/scripts':          { 'GET': { auth: auth2 } },
+  '/portfolio/performance':      { 'GET': { auth: auth2 } },
+  '/portfolio/summary':          { 'GET': { auth: auth2 } },
+  '/portfolio/symbol':           { 'GET': { auth: auth2 } },
+  '/holding-gains/distribution': { 'GET': { auth: auth2 } },
+  '/fo/positions':               { 'GET': { auth: auth2 } }
+};
