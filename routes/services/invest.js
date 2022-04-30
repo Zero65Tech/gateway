18d39f75ea5ec0b50116c2bba3dd46d598cd585e
Prashant <prashant@zero65.in>
2022-04-30 22:01:32 +0530

    Error handling while verifying id token

--- routes/services/invest.js
+++ routes/services/invest.js
@@ -6,9 +6,13 @@ async function auth(req) {
 
   let token = req.headers.authorization || req.cookies.authorization;
   if(token && token.startsWith('Bearer ')) {
-    req.query.account = token.length > 500 // TODO: Fix - Use ID tokens only
-      ? (await oAuth2Client.verifyIdToken({ idToken:token.substring('Bearer '.length), audience:'220251834863-p6gimkv0cgepodik4c1s8cs471dv9ioq.apps.googleusercontent.com' })).payload.email
-      : (await oAuth2Client.getTokenInfo(token.substring('Bearer '.length))).email;
+    try {
+      req.query.account = token.length > 500 // TODO: Fix - Use ID tokens only
+        ? (await oAuth2Client.verifyIdToken({ idToken:token.substring('Bearer '.length), audience:'220251834863-p6gimkv0cgepodik4c1s8cs471dv9ioq.apps.googleusercontent.com' })).payload.email
+        : (await oAuth2Client.getTokenInfo(token.substring('Bearer '.length))).email;
+    } catch(e) {
+      return false;
+    }
   } else {
     req.query.account = 'demo@zero65.in';
   }
