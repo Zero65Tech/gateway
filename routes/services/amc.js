97e0c8519be4a5ed9fde6809edcb5fe694a442de
Prashant <prashant@zero65.in>
2022-07-30 23:55:14 +0530

    Adding amc service config

---
+++ routes/services/amc.js
@@ -0,0 +1,17 @@
+const { OAuth2Client } = require('google-auth-library');
+const oAuth2Client = new OAuth2Client();
+
+
+
+async function auth(req) {
+  let token = req.headers.authorization;
+  if(token && token.startsWith('Bearer '))
+    return (await oAuth2Client.getTokenInfo(token.substring('Bearer '.length))).email == 'cloud-run@zero65.iam.gserviceaccount.com';
+  return false;
+}
+
+module.exports = {
+  '/portfolio': {
+    'GET': { auth: auth }
+  }
+}
