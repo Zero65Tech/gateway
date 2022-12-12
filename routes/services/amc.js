038f52b841106bddf7934fefa13376b011e2dd03
Prashant Gupta <antshpra@gmail.com>
2022-12-12 10:04:54 +0530

    AMC service config

--- routes/services/amc.js
+++ routes/services/amc.js
@@ -5,8 +5,10 @@ const oAuth2Client = new OAuth2Client();
 
 async function auth(req) {
   let token = req.headers.authorization;
-  if(token && token.startsWith('Bearer '))
-    return (await oAuth2Client.getTokenInfo(token.substring('Bearer '.length))).email == 'antshpra@gmail.com';
+  if(token && token.startsWith('Bearer ')) {
+    let email = (await oAuth2Client.getTokenInfo(token.substring('Bearer '.length))).email
+    return email == 'antshpra@gmail.com' || email == 'run-invest@zero65.iam.gserviceaccount.com';
+  }
   return false;
 }
 
