42bd476ecbf743792d32234c3c07dcd63274e81e
Prashant Gupta <antshpra@gmail.com>
2022-12-14 02:48:04 +0530

    invest service config

--- routes/services/invest.js
+++ routes/services/invest.js
@@ -57,6 +57,7 @@ module.exports = {
   '/trades/organized'           : { 'GET': { auth: auth } },
   '/sprints/summary'            : { 'GET': { auth: auth } },
   '/sprints/trades-v2'          : { 'GET': { auth: auth } },
+  '/portfolio-v4'               : { 'GET': { auth: auth } },
   '/portfolio-v4/amount'        : { 'GET': { auth: auth } },
   '/portfolio-v4/amount/set'    : { 'GET': { auth: auth } },
   '/portfolio-v4/pnl'           : { 'GET': { auth: auth } },
