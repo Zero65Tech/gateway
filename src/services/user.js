16dcda4e8917668e7aa314c09bb1b4cf35c94bda
Prashant Gupta <antshpra@gmail.com>
2023-01-22 10:24:33 +0530

    user service config

--- src/services/user.js
+++ src/services/user.js
@@ -1,11 +1,18 @@
-function auth(req, session) {
+function authGet(req, session) {
   if(!session)
     return false;
   req.query.sessionId = session.id;
   return true;
 }
 
+function authPost(req, session) {
+  if(!session || session != 'active')
+    return false;
+  req.body.sessionId = session.id;
+  return true;
+}
+
 module.exports = {
-  '/session'      : { 'GET': { auth: auth }, 'POST': {} },
-  '/google-login' : { 'POST': {} }
+  '/session'      : { 'GET': { auth: authGet }, 'POST': {} },
+  '/google-login' : { 'POST': { auth: authPost } }
 }
