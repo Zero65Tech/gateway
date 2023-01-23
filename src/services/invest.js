d081372334bcb8a35fc6e3c0d36ecccf378d0692
Prashant Gupta <antshpra@gmail.com>
2023-01-23 21:50:12 +0530

    invest service config

--- src/services/invest.js
+++ src/services/invest.js
@@ -1,64 +1,63 @@
-const { OAuth2Client } = require('google-auth-library');
-const { Service } = require('@zero65/utils');
-const oAuth2Client = new OAuth2Client();
+async function loggedinOnly(req, session) {
 
-async function auth(req, session) {
-
-  try {
-    if(req.headers.authorization) { // Chrome Extension
-      req.query.account = (await oAuth2Client.getTokenInfo(req.headers.authorization.substring('Bearer '.length))).email;
-    } else if(session && session.status == 'loggedin') { // Web Apps
-      req.query.userId = session.user.id;
-      let user = await Service.doGet('user', '/',  {}, { id: session.user.id });
-      req.query.account = user.email;
-    } else {
-      req.query.account = 'demo@zero65.in';
-    }
-  } catch(e) {
-    console.log(e);
+  if(!session && session.status != 'loggedin')
     return false;
-  }
 
+  req.query.userId = session.user.id;
+  
   return true;
 
 }
 
+async function loggedinOrDemo(req, session) {
+  if(session && session.status == 'loggedin')
+    req.query.userId = session.user.id;
+  else
+    req.query.account = 'demo@zero65.in';
+  return true;
+}
+
+
+
 module.exports = {
 
-  '/users/profiles'             : { 'GET': { auth: auth } },
-  '/users/groups'               : { 'GET': { auth: auth } },
-  '/trades'                     : { 'GET': { auth: auth } },
-  '/trades/fo/mom'              : { 'GET': { auth: auth } },
-  '/trades/pnl'                 : { 'GET': { auth: auth } },
-  '/transactions'               : { 'GET': { auth: auth } },
-  '/transactions/funds'         : { 'GET': { auth: auth } },
-  '/statement/dates'            : { 'GET': { auth: auth } },
-  '/statement/summary'          : { 'GET': { auth: auth } },
-  '/sprints/trades'             : { 'GET': { auth: auth } },
-  '/portfolio'                  : { 'GET': { auth: auth } },
-  '/portfolio/yoy'              : { 'GET': { auth: auth } },
-  '/portfolio/scripts'          : { 'GET': { auth: auth } },
-  '/portfolio/performance'      : { 'GET': { auth: auth } },
-  '/portfolio/summary'          : { 'GET': { auth: auth } },
-  '/portfolio/symbol'           : { 'GET': { auth: auth } },
+  '/users/profiles'             : { 'GET': { auth: loggedinOrDemo } },
+  '/users/groups'               : { 'GET': { auth: loggedinOrDemo } },
+  '/trades'                     : { 'GET': { auth: loggedinOrDemo } },
+  '/trades/fo/mom'              : { 'GET': { auth: loggedinOrDemo } },
+  '/trades/pnl'                 : { 'GET': { auth: loggedinOrDemo } },
+  '/transactions'               : { 'GET': { auth: loggedinOrDemo } },
+  '/transactions/funds'         : { 'GET': { auth: loggedinOrDemo } },
+  '/statement/dates'            : { 'GET': { auth: loggedinOrDemo } },
+  '/statement/summary'          : { 'GET': { auth: loggedinOrDemo } },
+  '/sprints/trades'             : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio'                  : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio/yoy'              : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio/scripts'          : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio/performance'      : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio/summary'          : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio/symbol'           : { 'GET': { auth: loggedinOrDemo } },
 
-  '/zerodha/ids'                : { 'GET': { auth: auth } },
-  '/zerodha/session'            : { 'GET': { auth: auth } },
-  '/user/sets'                  : { 'GET': { auth: auth } },
-  '/trades/scripts'             : { 'GET': { auth: auth } },
-  '/trades/about'               : { 'GET': { auth: auth } },
-  '/trades/organized'           : { 'GET': { auth: auth } },
-  '/sprints/summary'            : { 'GET': { auth: auth } },
-  '/sprints/trades-v2'          : { 'GET': { auth: auth } },
-  '/portfolio-v4'               : { 'GET': { auth: auth } },
-  '/portfolio-v4/dates'         : { 'GET': { auth: auth } },
-  '/portfolio-v4/amount'        : { 'GET': { auth: auth } },
-  '/portfolio-v4/amount/set'    : { 'GET': { auth: auth } },
-  '/portfolio-v4/pnl'           : { 'GET': { auth: auth } },
-  '/portfolio-v4/pnl-tax'       : { 'GET': { auth: auth } },
-  '/portfolio-v4/fo'            : { 'GET': { auth: auth } },
 
-  '/holding-gains/distribution' : { 'GET': { auth: auth } },
-  '/fo/positions'               : { 'GET': { auth: auth } },
+  '/zerodha/ids'                : { 'GET': { auth: loggedinOnly } },
+  '/zerodha/session'            : { 'GET': { auth: loggedinOnly } },
+
+  '/user/sets'                  : { 'GET': { auth: loggedinOrDemo } },
+  '/trades/scripts'             : { 'GET': { auth: loggedinOrDemo } },
+  '/trades/about'               : { 'GET': { auth: loggedinOrDemo } },
+  '/trades/organized'           : { 'GET': { auth: loggedinOrDemo } },
+  '/sprints/summary'            : { 'GET': { auth: loggedinOrDemo } },
+  '/sprints/trades-v2'          : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio-v4'               : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio-v4/dates'         : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio-v4/amount'        : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio-v4/amount/set'    : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio-v4/pnl'           : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio-v4/pnl-tax'       : { 'GET': { auth: loggedinOrDemo } },
+  '/portfolio-v4/fo'            : { 'GET': { auth: loggedinOrDemo } },
+
+
+  '/holding-gains/distribution' : { 'GET': { auth: loggedinOrDemo } },
+  '/fo/positions'               : { 'GET': { auth: loggedinOrDemo } },
   
 };
--- src/services/zerodha.js
+++ src/services/zerodha.js
@@ -10,8 +10,7 @@ const userAccess = {
   'nextshivendra@gmail.com':   [ 'LV0248', 'IQ6382', 'NH6133', 'AJ8534', 'PC3199' ],
   'shivendraec1088@gmail.com': [ 'LV0248', 'IQ6382', 'NH6133', 'AJ8534', 'PC3199' ],
 
-  'antshpra@gmail.com':                        [ 'LV0248', 'IQ6382', 'NH6133', 'AJ8534', 'PC3199', 'TF3445', 'IMK762' ],
-  'run-invest@zero65.iam.gserviceaccount.com': [ 'LV0248', 'IQ6382', 'NH6133', 'AJ8534', 'PC3199', 'TF3445', 'IMK762' ],
+  'antshpra@gmail.com': [ 'LV0248', 'IQ6382', 'NH6133', 'AJ8534', 'PC3199', 'TF3445', 'IMK762' ],
 
 }
 
@@ -25,9 +24,6 @@ async function auth(req) {
 }
 
 module.exports = {
-  '/session': {
-    'GET': { auth: auth }
-  },
   '/enctoken': {
     'GET': { auth: auth }
   },
