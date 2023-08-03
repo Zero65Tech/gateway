beb06865e1842801c306448606834c0422d6f467
Prashant Gupta <antshpra@gmail.com>
2023-08-03 17:10:59 +0530

    config

--- src/config.js
+++ src/config.js
@@ -1,23 +1,9 @@
-const cluster = '2fn4cwcjeq-uc';
-
-const helloApp = [
-  [ '/assets/', null, `hello-vue3-${ cluster }.a.run.app`, path => path ],
-  [ '/images/', null, `hello-vue3-${ cluster }.a.run.app`, path => path ],
-  [ '/css/',    null, `hello-vue3-${ cluster }.a.run.app`, path => path ],
-  [ '/js/',     null, `hello-vue3-${ cluster }.a.run.app`, path => path ],
-  [ '',         null, `hello-vue3-${ cluster }.a.run.app`, path => '/'  ],
-];
-
-
-
 try {
 
-  let config = require('@zero65/config');
-  config[ 'hello.zero65.in' ] = helloApp;
-  module.exports = config;
+  module.exports = require('@zero65/config');
 
 } catch (e) {
 
-  module.exports = { 'hello.zero65.in': helloApp };
+  module.exports = {};
 
 }
